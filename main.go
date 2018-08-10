879762ba2442eb6229ca421d470a3e7ef0ca8410
tomocy <tomocy.dev@gmail.com>
2018-08-10 16:29:50 +0900

    fix

--- main.go
+++ main.go
@@ -15,19 +15,34 @@ var cmdChain = []*exec.Cmd{
 }
 
 func main() {
+	setUpCommandChain()
+
+	startAndWaitAllCommands()
+}
+
+func setUpCommandChain() {
 	cmdChain[0].Stdin = os.Stdin
 	cmdChain[len(cmdChain)-1].Stdout = os.Stdout
 	for i := 0; i < len(cmdChain)-1; i++ {
 		cmd := cmdChain[i]
 		nextCmd := cmdChain[i+1]
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			log.Panicf("could not get stdout pipe: %s\n", err)
+		if err := connectPipe(cmd, nextCmd); err != nil {
+			log.Panicf("could not connect pipe: %s\n", err)
 		}
+	}
+}
 
-		nextCmd.Stdin = stdout
+func connectPipe(cmd *exec.Cmd, nextCmd *exec.Cmd) error {
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
 	}
 
+	nextCmd.Stdin = stdout
+	return nil
+}
+
+func startAndWaitAllCommands() {
 	for _, cmd := range cmdChain {
 		if err := cmd.Start(); err != nil {
 			log.Panicf("could not start command: %s\n", err)
