a75950f66e44bcada4f7db2d947b4eb810a4105e
tomocy <tomocy.dev@gmail.com>
2018-08-10 15:28:20 +0900

    add build.sh to set up

---
+++ build.sh
@@ -0,0 +1,21 @@
+echo build domainfinder
+go build -o domainfinder
+
+echo build synonyms
+cd ../synonyms
+go build -o ../domainfinder/lib/synonyms
+
+echo build sprinkle
+cd ../sprinkle
+go build -o ../domainfinder/lib/sprinkle
+
+echo build coolify
+cd ../coolify
+go build -o ../domainfinder/lib/coolify
+
+echo build domainify
+cd ../domainify
+go build -o ../domainfinder/lib/domainify
+
+echo build available
+go build -o ../domainfinder/lib/available
